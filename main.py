fa51ab67ec6ad50c42fa7966c84ca25b0a6ca991
“rashaamro” <rashaamro3@gmail.com>
2023-08-02 15:01:21 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+name = input("input youe full name :")
+mobile_number = input("enter your phone number LIKE 05X-xxx-xxx")
+mob = mobile_number.split("-")
+year = int(input("enter year of birth:"))
+age = 2023 - year
+
+print(name)
+print(mob)
+print(age)
